8e7ed6ab3097de6cf529b04b0db286ecbe14b47c
xbluecode <besbes.af@gmail.com>
2019-11-08 15:42:24 +0200

    Initializing spring boot application

---
+++ src/main/java/imoovo/javadev/JavadevApplication.java
@@ -0,0 +1,13 @@
+package imoovo.javadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadevApplication.class, args);
+	}
+
+}
