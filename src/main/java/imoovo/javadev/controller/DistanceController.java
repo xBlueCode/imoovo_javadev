e335bea08fb4c2410f70c5fdb6b72d7b983f0b14
xbluecode <besbes.af@gmail.com>
2019-11-08 17:40:16 +0200

    imp: distance controller, endpoint: dist/history

--- src/main/java/imoovo/javadev/controller/DistanceController.java
+++ src/main/java/imoovo/javadev/controller/DistanceController.java
@@ -6,16 +6,13 @@ import imoovo.javadev.service.UserServiceDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/dist")
+@RequestMapping("/api/dist/")
 public class DistanceController {
 
 	private UserServiceDb userService;
@@ -43,5 +40,45 @@ public class DistanceController {
 		body.put("direct_distance", distance.getDist());
 		body.put("unit", "km");
 		return new ResponseEntity<>(body, HttpStatus.OK);
+	}
+
+	@GetMapping("history")
+	public ResponseEntity<?> history(@RequestParam String username, @RequestParam(required = false) String forName)
+	{
+		HashMap<String, Object> body = new HashMap<>();
+		Optional<User> opUser = userService.findUserByUsername(username);
+		HttpStatus code;
+		if (!opUser.isPresent())
+		{
+			code = HttpStatus.BAD_REQUEST;
+			body.put("error", String.format("The user %s doesn't exist", forName));
+		}
+		else if (forName == null)
+		{
+			code = HttpStatus.OK;
+			body.put("user", username);
+			body.put("calculations:", opUser.get().getDistances());
+		}
+		else if (!opUser.get().getRole().equals("admin"))
+		{
+			code = HttpStatus.FORBIDDEN;
+			body.put("error", "User must be admin to perform this action");
+		}
+		else{
+			Optional<User> targetedUser = userService.findUserByUsername(forName);
+			if (!targetedUser.isPresent())
+			{
+				code = HttpStatus.BAD_REQUEST;
+				body.put("error", String.format("The user %s doesn't exist", forName));
+			}
+			else
+			{
+				code = HttpStatus.OK;
+				body.put("user", forName);
+				body.put("calculations:", targetedUser.get().getDistances());
+			}
+		}
+		body.put("code", code);
+		return new ResponseEntity<>(body, code);
 	}
 }
